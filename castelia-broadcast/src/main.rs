c085c4a4c3ec7c940c253ccc83390d2f9d1d6892
yum <contact@meyume.com>
2025-12-07 18:38:07 -0500

    init: set up broadcast server

--- castelia-broadcast/src/main.rs
+++ castelia-broadcast/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
