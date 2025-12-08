521984e9c52971e72777cdc36edf20bda9698b02
yum <contact@meyume.com>
2025-12-07 20:26:49 -0500

    feat: set up logging

--- castelia-broadcast/src/main.rs
+++ castelia-broadcast/src/main.rs
@@ -1,10 +1,14 @@
 use axum::{Router, routing::get};
+use tracing::info;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    info!("Listing on {}", listener.local_addr()?);
+
     axum::serve(listener, app).await?;
 
     Ok(())
