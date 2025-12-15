94f059cdbdc0ac86b3850c3e96d9af6ae03f090c
yum <contact@meyume.com>
2025-12-15 11:09:32 -0500

    refactor(rtmp/router): update error handling for bad netconnection messages

--- castelia-rtmp/src/messages/router.rs
+++ castelia-rtmp/src/messages/router.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 use thiserror::Error;
 use tokio::sync::mpsc::Sender;
-use tracing::{trace, warn};
+use tracing::trace;
 
 use crate::{
     messages::{Message, command::CommandMessage},
@@ -12,7 +12,12 @@ use crate::{
 };
 
 #[derive(Error, Debug)]
-pub enum RouteError {}
+pub enum RouteError {
+    #[error("Message was routed to message stream {0}, which does not exist")]
+    MissingNetStream(u32),
+    #[error("Netconnection commands must be issued on message stream 0, found {0}")]
+    InvalidNetconnectionRoute(u32),
+}
 
 pub struct MessageRouter {
     net_connection: NetConnection,
@@ -40,29 +45,36 @@ impl MessageRouter {
         match message {
             Message::Protocol(protocol_control_message) => {
                 trace!("routing message to netconnection");
+                if message_stream_id != 0 {
+                    return Err(RouteError::InvalidNetconnectionRoute(message_stream_id));
+                }
                 self.net_connection
                     .handle_protocol_message(protocol_control_message, send_queue)
                     .await
             }
             Message::UserControl(user_control_message) => {
                 trace!("routing message to netconnection");
+                if message_stream_id != 0 {
+                    return Err(RouteError::InvalidNetconnectionRoute(message_stream_id));
+                }
                 self.net_connection
                     .handle_user_control_message(user_control_message, send_queue)
                     .await
             }
             Message::Command(command_message) => {
-                if message_stream_id == 0 {
-                    if let CommandMessage::NetConnection(command) = command_message {
-                        trace!("routing message to netconnection");
-                        self.net_connection
-                            .handle_command(command, send_queue)
-                            .await;
-                    } else {
-                        warn!("netconnection could not handle command: {command_message:?}");
+                if let CommandMessage::NetConnection(command) = command_message {
+                    trace!("routing message to netconnection");
+                    if message_stream_id != 0 {
+                        return Err(RouteError::InvalidNetconnectionRoute(message_stream_id));
                     }
+                    self.net_connection
+                        .handle_command(command, send_queue)
+                        .await;
                 } else {
                     trace!("routing message to netstream {message_stream_id}");
-                    let stream = self.message_streams.get(&message_stream_id);
+                    let Some(stream) = self.message_streams.get(&message_stream_id) else {
+                        return Err(RouteError::MissingNetStream(message_stream_id));
+                    };
                 }
             }
         }
