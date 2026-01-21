03952ae0a6694fdf2a21c28b7367d2fe9c1db8a7
yum <contact@meyume.com>
2026-01-20 22:39:19 -0500

    [auth]: create signup endpoint

--- castelia-auth/src/routes/mod.rs
+++ castelia-auth/src/routes/mod.rs
@@ -1,13 +1,85 @@
 use std::sync::Arc;
 
+use argon2::{
+    Argon2,
+    password_hash::{PasswordHashString, PasswordHasher, SaltString, rand_core::OsRng},
+};
 use axum::{
-    Router,
-    extract::State,
+    Json, Router,
+    extract::{FromRequest, State, rejection::JsonRejection},
     http::StatusCode,
+    response::{IntoResponse, Response},
     routing::{get, post},
 };
+use serde::Deserialize;
+use thiserror::Error;
+use tracing::error;
+use validator::Validate;
 
 use crate::AppState;
+
+#[derive(Deserialize, Validate)]
+struct CreateUser {
+    #[validate(length(min = 3, max = 32))]
+    username: String,
+    #[validate(email)]
+    email: String,
+    #[validate(length(min = 6))]
+    password: String,
+}
+
+struct ValidatedUser(CreateUser);
+impl<S> FromRequest<S> for ValidatedUser
+where
+    S: Send + Sync,
+{
+    type Rejection = CreateUserError;
+
+    async fn from_request(req: axum::extract::Request, state: &S) -> Result<Self, Self::Rejection> {
+        let Json(user) = Json::<CreateUser>::from_request(req, state).await?;
+        user.validate()?;
+        Ok(ValidatedUser(user))
+    }
+}
+
+#[derive(Debug, Error)]
+enum CreateUserError {
+    #[error(transparent)]
+    ValidationError(#[from] validator::ValidationErrors),
+
+    #[error("Failed to hash password")]
+    PasswordHashFailure,
+
+    #[error(transparent)]
+    JsonRejection(#[from] JsonRejection),
+
+    #[error(transparent)]
+    InsertionError(#[from] sqlx::Error),
+}
+
+impl IntoResponse for CreateUserError {
+    fn into_response(self) -> Response {
+        match self {
+            CreateUserError::ValidationError(validation_errors) => {
+                (StatusCode::BAD_REQUEST, validation_errors.to_string()).into_response()
+            }
+            CreateUserError::PasswordHashFailure => {
+                error!("Failed to hash password");
+                StatusCode::INTERNAL_SERVER_ERROR.into_response()
+            }
+            CreateUserError::JsonRejection(json_rejection) => json_rejection.into_response(),
+            CreateUserError::InsertionError(error) => match error {
+                sqlx::Error::InvalidArgument(error) => {
+                    (StatusCode::BAD_REQUEST, error).into_response()
+                }
+                _ => {
+                    error!("Failed to insert user into database: {error}");
+                    StatusCode::INTERNAL_SERVER_ERROR.into_response()
+                }
+            },
+        }
+    }
+}
 
 pub fn router(state: Arc<AppState>) -> Router {
     Router::new()
@@ -22,7 +94,32 @@ async fn health_check() -> StatusCode {
     StatusCode::OK
 }
 
-async fn signup(State(state): State<Arc<AppState>>) {}
+async fn signup(
+    State(state): State<Arc<AppState>>,
+    ValidatedUser(user): ValidatedUser,
+) -> Result<(), CreateUserError> {
+    let password_hash =
+        tokio::task::spawn_blocking(move || -> Result<PasswordHashString, CreateUserError> {
+            let salt = SaltString::generate(&mut OsRng);
+            let password_hash = Argon2::default()
+                .hash_password(user.password.as_bytes(), &salt)
+                .map_err(|_| CreateUserError::PasswordHashFailure)?;
+            Ok(password_hash.serialize())
+        })
+        .await
+        .map_err(|_| CreateUserError::PasswordHashFailure)??;
+
+    sqlx::query!(
+        r#"INSERT INTO users (username, email, password) VALUES ($1, $2, $3)"#,
+        user.username,
+        user.email,
+        password_hash.as_str()
+    )
+    .execute(&state.db_pool)
+    .await?;
+
+    Ok(())
+}
 
 async fn login() {}
 
---
+++ migrations/20260121020154_users.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username TEXT UNIQUE NOT NULL,
+    email VARCHAR(320) NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
