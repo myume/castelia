6132b9d5b9a7fdb1d6da55ae5405736c52037a39
yum <contact@meyume.com>
2026-01-27 15:04:36 -0500

    fix: correct hls extension for ffmpeg

--- castelia-transcoder/src/transcoder.rs
+++ castelia-transcoder/src/transcoder.rs
@@ -82,7 +82,7 @@ impl TranscoderService {
             .arg("aac")
             .arg("-f")
             .arg("hls")
-            .arg(format!("{}/stream.m3m8", hls_output_dir.display()));
+            .arg(format!("{}/stream.m3u8", hls_output_dir.display()));
 
         let child = command.spawn().context("Failed to spawn ffmpeg process")?;
         self.processes.insert(stream_id.to_owned(), child);
