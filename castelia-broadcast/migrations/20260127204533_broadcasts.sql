c07699150a477356fe57f58fcbb159872228dece
yum <contact@meyume.com>
2026-01-27 20:21:58 -0500

    feat: setup broadcast initialization

--- castelia-broadcast/migrations/20260127204533_broadcasts.sql
+++ castelia-broadcast/migrations/20260127204533_broadcasts.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS broadcasts(
     id SERIAL PRIMARY KEY,
     channel_name TEXT UNIQUE NOT NULL,
     title TEXT DEFAULT '' NOT NULL,
-    start_time TIMESTAMP,
+    start_time TIMESTAMPTZ,
     status TEXT DEFAULT 'offline' NOT NULL,
     private BOOLEAN DEFAULT false NOT NULL
 );
--- castelia-broadcast/src/routes/broadcasts.rs
+++ castelia-broadcast/src/routes/broadcasts.rs
@@ -1,44 +1,121 @@
-use std::path::Path;
-
-use axum::{Json, Router, extract::State, http::StatusCode, routing::post};
-use serde::Deserialize;
+use axum::{
+    Json, Router,
+    extract::{Path, State},
+    http::StatusCode,
+    response::IntoResponse,
+    routing::get,
+};
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use sqlx::Connection;
 use tower_http::services::ServeDir;
-use tracing::error;
+use tracing::{debug, error};
 
 use crate::AppState;
 
-#[derive(Deserialize)]
-pub struct InitializeBroadcast {
+#[derive(Serialize, Deserialize)]
+pub struct Broadcast {
     channel_name: String,
+    title: String,
+    start_time: Option<DateTime<Utc>>,
+    status: String,
+    private: bool,
 }
 
-pub fn router(hls_dir: &Path) -> Router<AppState> {
+#[derive(Debug, thiserror::Error)]
+enum GetBroadcastError {
+    #[error(transparent)]
+    Database(#[from] sqlx::Error),
+
+    #[error("Failed to initialize broadcast: {0}")]
+    InitializeBroadcast(sqlx::Error),
+
+    #[error("Could not fetch broadcast: {0}")]
+    FetchBroadcast(sqlx::Error),
+}
+
+impl IntoResponse for GetBroadcastError {
+    fn into_response(self) -> axum::response::Response {
+        match self {
+            GetBroadcastError::InitializeBroadcast(error)
+            | GetBroadcastError::FetchBroadcast(error)
+            | GetBroadcastError::Database(error) => {
+                error!("{error}");
+                StatusCode::INTERNAL_SERVER_ERROR.into_response()
+            }
+        }
+    }
+}
+#[derive(Serialize, Deserialize)]
+pub struct UpdateBroadcast {
+    title: Option<String>,
+    status: Option<String>,
+    private: Option<bool>,
+}
+
+pub fn router(hls_dir: &std::path::Path) -> Router<AppState> {
     Router::new()
-        .route("/broadcasts/initialize", post(initialize_broadcast))
-        .nest_service("/broadcasts", ServeDir::new(hls_dir))
+        .route(
+            "/broadcasts/{channel}",
+            get(get_broadcast).patch(update_broadcast),
+        )
+        .nest_service("/broadcasts/hls", ServeDir::new(hls_dir))
 }
 
-async fn initialize_broadcast(
+async fn update_broadcast(
     State(state): State<AppState>,
-    Json(req): Json<InitializeBroadcast>,
+    Path(channel_name): Path<String>,
+    Json(req): Json<UpdateBroadcast>,
 ) -> StatusCode {
     if let Err(e) = sqlx::query!(
-        r#"INSERT INTO broadcasts (channel_name) VALUES($1)"#,
-        req.channel_name
+        "UPDATE broadcasts SET title = $1, status = $2, private = $3 
+        WHERE channel_name = $4",
+        req.title,
+        req.status,
+        req.private,
+        channel_name,
     )
     .execute(&state.db)
     .await
     {
-        if let Some(e) = e.as_database_error()
-            && e.is_unique_violation()
-        {
-            error!("Broadcast already exists");
-            return StatusCode::BAD_REQUEST;
-        }
-
-        error!("Failed to create broadcast: {e}");
+        error!("Failed to update broadcast metadata: {e}");
         return StatusCode::INTERNAL_SERVER_ERROR;
     }
 
-    StatusCode::CREATED
+    StatusCode::OK
+}
+
+async fn get_broadcast(
+    State(state): State<AppState>,
+    Path(channel_name): Path<String>,
+) -> Result<Json<Broadcast>, GetBroadcastError> {
+    let mut conn = state.db.acquire().await?;
+    let mut tx = conn.begin().await?;
+
+    let fetch_result = sqlx::query_as!(
+        Broadcast,
+        "SELECT channel_name, title, start_time, status, private FROM broadcasts WHERE channel_name = $1",
+        channel_name
+    ).fetch_optional(&mut *tx).await;
+
+    let broadcast = match fetch_result {
+        Ok(Some(broadcast)) => broadcast,
+        Ok(None) => {
+            debug!("broadcast not found for {channel_name}, initializing...");
+            sqlx::query_as!(
+                Broadcast,
+                "INSERT INTO broadcasts (channel_name) VALUES($1)
+                 RETURNING channel_name, title, start_time, status, private",
+                channel_name
+            )
+            .fetch_one(&mut *tx)
+            .await
+            .map_err(GetBroadcastError::InitializeBroadcast)?
+        }
+        Err(e) => return Err(GetBroadcastError::FetchBroadcast(e)),
+    };
+
+    tx.commit().await?;
+
+    Ok(Json(broadcast))
 }
