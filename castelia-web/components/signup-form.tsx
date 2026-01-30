009f40b0db89cefbc72b27c56e8e3b6f73df5cb0
yum <contact@meyume.com>
2026-01-29 22:23:53 -0500

    feat: implement signup

--- castelia-web/components/signup-form.tsx
+++ castelia-web/components/signup-form.tsx
@@ -4,25 +4,53 @@ import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
+  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useAuth } from "@/providers/auth-provider";
+import { redirect } from "next/navigation";
+import { useState } from "react";
 
 export function SignupForm() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
+  const [signupError, setSignupError] = useState("");
+  const { login } = useAuth();
+
+  const signup = async (formData: FormData) => {
     const username = formData.get("username");
     const email = formData.get("email");
     const password = formData.get("password");
-    console.log({ username, email, password });
+    if (!username || !email || !password) {
+      setSignupError("Username, email, and password are required.");
+      return;
+    }
+    const response = await fetch("/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({ username, email, password }),
+    });
+
+    if (!response.ok) {
+      setSignupError(`Failed to signup: ${await response.text()}`);
+      return;
+    }
+    try {
+      await login(username.toString(), password.toString());
+    } catch (e) {
+      console.error(e);
+      return;
+    }
+    redirect("/");
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form action={signup}>
       <Card className="w-96">
         <CardHeader>
           <CardTitle>Sign Up</CardTitle>
@@ -35,6 +63,7 @@ export function SignupForm() {
               name="username"
               placeholder="Your username"
               minLength={3}
+              required
             />
           </div>
           <div className="space-y-2">
@@ -44,6 +73,7 @@ export function SignupForm() {
               name="email"
               type="email"
               placeholder="you@example.com"
+              required
             />
           </div>
           <div className="space-y-2">
@@ -53,8 +83,14 @@ export function SignupForm() {
               name="password"
               type="password"
               minLength={6}
+              required
             />
           </div>
+          {signupError && (
+            <CardDescription className="text-red-500">
+              {signupError}
+            </CardDescription>
+          )}
         </CardContent>
         <CardFooter>
           <Button type="submit">Sign Up</Button>
